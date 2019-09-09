009ee9c21804322397526649957e4bb641068415
Emmanuel Tweneboah <twentekghana@gmail.com>
2019-09-09 14:35:18 +0800

    mongoose setup

---
+++ v3/app.js
@@ -0,0 +1,30 @@
+ const express = require('express');
+const mongoose = require('mongoose');
+ const app = express();
+
+//MIDDLEWARE
+app.set('view engine', 'ejs');
+
+
+//DB CONNECTION
+const url =  'mongodb://localhost/my-YouTuBe'
+mongoose.connect( url, {
+useNewUrlParser: true,
+useCreateIndex: true,
+useFindAndModify: false
+})
+.then(() => console.log("DB Connected successfully"));
+
+
+
+//ROUTES
+app.get('/', (req, res) => {
+ res.render('home')
+})
+
+
+//SEVER
+let PORT =  process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`My Youtube App Server is runing on port ${PORT}`)
+})
